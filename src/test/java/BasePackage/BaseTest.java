c09db4a040bf6ec032d538917879ca4e27a91e33
Bhoopathy <bhoopathy9051@gmail.com>
2026-05-15 16:20:47 +0530

    Changed AfterMethod to AfterClass in BaseTest

--- src/test/java/BasePackage/BaseTest.java
+++ src/test/java/BasePackage/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -21,7 +22,7 @@ public class BaseTest {
        driver.manage().window().maximize();
        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
    }
-@AfterMethod
+@AfterClass
     public void tearDown(){
        driver.quit();
 }
