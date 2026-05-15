21952ede286e5b244ba74b4dd24657397d34acf5
Bhoopathy <bhoopathy9051@gmail.com>
2026-05-15 19:23:53 +0530

    Test for jenkins Job

--- src/test/java/BasePackage/BaseTest.java
+++ src/test/java/BasePackage/BaseTest.java
@@ -21,6 +21,7 @@ public class BaseTest {
 
        driver.manage().window().maximize();
        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+       // New Code Changes
    }
 @AfterClass
     public void tearDown(){
