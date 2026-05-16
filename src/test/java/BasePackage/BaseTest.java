84e8befad424aea32181a7aa718d5fc1dcc8ca5a
Bhoopathy <bhoopathy9051@gmail.com>
2026-05-16 20:11:42 +0530

    commit

--- src/test/java/BasePackage/BaseTest.java
+++ src/test/java/BasePackage/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -20,10 +21,13 @@ public class BaseTest {
 
        driver.manage().window().maximize();
        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+       // New Code Changes
    }
-@AfterMethod
+@AfterClass
     public void tearDown(){
        driver.quit();
+
+       //CiCd Run Check
 }
 
 }
